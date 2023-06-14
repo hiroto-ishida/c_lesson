39a531bcda8a7ae3f3340b59e2dbf976d67cbbbb
Hiroto Ishida <ombossk0720@gmail.com>
2023-06-15 01:19:50 +0900

    Modification

--- practice1.c
+++ practice1.c
@@ -6,16 +6,14 @@ int main(){
   
   printf("国語の点数は？");
   scanf("%d",&japanese);
-  total = japanese;
   
   printf("社会の点数は？");
   scanf("%d",&social);
-  total += social;
   
   printf("理科の点数は？");
   scanf("%d",&science);
-  total += science;
 
+  total = japanese + social + science;
   average = total / 3;
 
   printf("合計 = %d, 平均 = %d ",total,average);

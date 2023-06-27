be0c17c5b04851fdca739955dc0fed08d2337be8
Hiroto Ishida <ombossk0720@gmail.com>
2023-06-27 22:17:20 +0900

    lesson2

--- practice2.c
+++ practice2.c
@@ -1,10 +1,12 @@
+// 練習問題2
+
 #include <stdio.h>
 
 int main () {
 
   int price,count,price_including_tax,base_price = 0;
   float tax = 1.1;
-  
+
   printf("単価を入力してください\n");
   scanf("%d",&price);
   printf("数量を入力してください\n");

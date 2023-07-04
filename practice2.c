6286fa97c0f450423ee76809687e011ad3715981
Hiroto Ishida <ombossk0720@gmail.com>
2023-07-04 20:02:37 +0900

    fix

--- practice2.c
+++ practice2.c
@@ -2,20 +2,23 @@
 // 練習問題2
 
 #include <stdio.h>
+#include <math.h>
+
+#define TAX 10
 
 int main () {
 
-  int price,count,price_including_tax,base_price = 0;
-  float tax = 1.1;
+  int price, count;
+  double base_price;
 
   printf("単価を入力してください\n");
-  scanf("%d",&price);
+  scanf("%d", &price);
   printf("数量を入力してください\n");
-  scanf("%d",&count);
+  scanf("%d", &count);
 
   base_price = price * count;
-  price_including_tax = base_price * tax;
 
-  printf("税抜き価格は%d円で、税込価格は%d円です",base_price,price_including_tax);
+  printf("税抜き価格は%.0f円で、税込価格は%.0f円です\n", base_price, round(base_price * (1 + TAX / 100.0)));
 
+  return 0;
 }

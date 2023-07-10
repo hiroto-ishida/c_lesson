76648950b7d0e405beb04e2ab6eb1f079c7bddb2
Hiroto Ishida <ombossk0720@gmail.com>
2023-07-10 23:23:20 +0900

    fix

--- practice3.c
+++ practice3.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <math.h>
+
 #define TAX 10.0
+#define DIV_CHILD 1
+#define DIV_ADULT 2
+#define PRICE_CHILD 800
+#define PRICE_ADULT 1500
 
-
-int main () {
-  int category,count = 0;
-  float base_price = 0.0;
-
-  printf("区分を入力してください。(1:子ども、2:大人):\n");
-  scanf("%d",&category);
+int main(void)
+{
+  int division = 0;
+  int count = 0;
+  int price = 0;
   
-  printf("人数を入力してください。:\n");
-  scanf("%d",&count);
+  printf("区分を入力してください。(1:子ども、2:大人):\n");
+  scanf("%d", &division);
 
-  if(category == 1){
-    base_price = count * 800;
-  } else if(category == 2) {
-    base_price = count * 1500;
-  } else {
+  printf("人数を入力してください。:\n");
+  scanf("%d", &count);
+
+  if(division == DIV_CHILD)
+  {
+    price = count * PRICE_CHILD;
+  }
+  else if (division == DIV_ADULT)
+  {
+    price = count * PRICE_ADULT;
+  }
+  else
+  {
     printf("1と2以外の区分が入力されました。\n");
   }
 
-  printf("金額(税抜)は%.0f円で\n",base_price);
+  printf("金額(税抜)は%d円で\n", price);
 
-  base_price = round(base_price * (1 + TAX / 100.0));
+  price = round(price * (1 + TAX / 100.0));
 
-  printf("税込価格は%.0f円です\n",base_price);
+  printf("税込価格は%d円です\n", price);
+
+
+  return 0;
 }
